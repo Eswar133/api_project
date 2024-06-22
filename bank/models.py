a7473845c1ea8f1549f5f69a0379da316a6eba83
Eswar <manikantapadala358@gmail.com>
2024-06-22 10:09:19 +0530

    6/22/2024

--- bank/models.py
+++ bank/models.py
@@ -1,20 +1,35 @@
-from django.db import models 
+from django.db import models
 
 class Bank(models.Model):
-    bank_id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=255,unique=True)
+    bank_id = models.BigAutoField(primary_key=True)  
+    name = models.CharField(max_length=49)
     
+
     def __str__(self):
-        return self.name 
-    
+        return self.name
+
 class Branch(models.Model):
-    ifsc = models.CharField(max_length=11,primary_key=True)
-    bank = models.ForeignKey(Bank,on_delete=models.CASCADE)
-    branch_name = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    district = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    
+    ifsc = models.CharField(max_length=11, primary_key=True)
+    bank = models.ForeignKey(Bank, on_delete=models.CASCADE)
+    branch_name = models.CharField(max_length=74)
+    address = models.CharField(max_length=195)  
+    city = models.CharField(max_length=50)  
+    district = models.CharField(max_length=50) 
+    state = models.CharField(max_length=26)  
+
     def __str__(self):
         return self.branch_name
+
+
+class AllData(models.Model):
+    ifsc = models.CharField(max_length=11)
+    bank_id = models.IntegerField()
+    branch = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    district = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    bank_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.bank_name
