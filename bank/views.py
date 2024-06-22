dac36be685292499ea369755f22e494cc3734e93
Eswar <manikantapadala358@gmail.com>
2024-06-22 12:40:52 +0530

    6/22/2024

--- bank/views.py
+++ bank/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse, HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
+from .models import Bank
 
-# Create your views here.
+@csrf_exempt
+def create_bank(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            return HttpResponseBadRequest("Invalid JSON")
+
+        name = data.get('name')
+        if not name:
+            return HttpResponseBadRequest("Missing 'name' field")
+
+        bank = Bank(name=name)
+        bank.save()
+
+        response_data = {
+            'bank_id': bank.bank_id,
+            'name': bank.name,
+        }
+
+        return JsonResponse(response_data, status=201)
+
+    return HttpResponseBadRequest("Only POST requests are allowed")
+
+@csrf_exempt
+def get_bank(request, bank_id):
+    try:
+        bank = Bank.objects.get(pk=bank_id)
+        response_data = {
+            'bank_id': bank.bank_id,
+            'name': bank.name,
+        }
+        return JsonResponse(response_data)
+    except Bank.DoesNotExist:
+        return HttpResponseNotFound("Bank not found")
